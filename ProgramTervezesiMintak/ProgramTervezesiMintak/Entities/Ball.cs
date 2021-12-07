c411b84e2e3f2c1d629b75e24297e897485a4a64
tommg01corvinus <tamas.gombkoto@stud.uni-corvinus.hu>
2021-12-07 23:59:05 +0100

    Initial commit

---
+++ ProgramTervezesiMintak/ProgramTervezesiMintak/Entities/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramTervezesiMintak.Entities
+{
+    class Ball: Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), 0, 0, Width, Height);
+        }
+
+        void MoveBall()
+        {
+            Left++;
+        }
+    }
+}
